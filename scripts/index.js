f74a1157eb31623a4b70a4b2b3e1072835d57416
Izan Khan <90479591+mayo850@users.noreply.github.com>
2023-11-08 10:54:24 -0600

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -232,14 +232,24 @@ window.addEventListener("load", event => {
     /* end */
 });
 
-
-const jobTitles = ["Engineer", "Artist", "Auditor"];
+const jobTitles = ["Engineer", "Artist", "Auditor", "Professor", "Teacher", "Coach", "PhD Candidate"];
 const jobTitleElement = document.getElementById("job-title");
 let currentIndex = 0;
 
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 function updateJobTitle() {
     jobTitleElement.textContent = jobTitles[currentIndex];
+    jobTitleElement.style.color = getRandomColor(); // Set random color
     currentIndex = (currentIndex + 1) % jobTitles.length;
 }
 
-setInterval(updateJobTitle, 2000); // Change job title every 2 seconds
+setInterval(updateJobTitle, 2000); // Change job title and color every 2 seconds
+
