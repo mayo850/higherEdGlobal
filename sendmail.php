3613d7be3235bf3d25c02d7584d1f1a02141fa9e
Izan Khan <90479591+mayo850@users.noreply.github.com>
2024-01-03 19:04:08 -0600

    Add files via upload

---
+++ sendmail.php
@@ -0,0 +1,16 @@
+﻿<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $to = "higheredg@gmail.com"; // Replace with your email address
+    $subject = "New Contact Form Submission";
+
+    $headers = "From: $name <$email>" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+
+    mail($to, $subject, $message, $headers);
+}
+?>
