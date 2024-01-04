b6296ad78479b98ed77970eb9e0705364153b7cb
Izan Khan <90479591+mayo850@users.noreply.github.com>
2024-01-03 19:03:17 -0600

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -46,32 +46,32 @@ window.addEventListener("load", event => {
     /* The Team */
     var team = [{
         name: "Haider Shah",
-        role: "Depaul University",
-        desc: "Originally from Pakistan, pursued his dream of studying business analytics in the United States with our support. After successfully completing his Master's at DePaul University, he seamlessly transitioned into the professional world. Haider is now thriving in his career at Deloitte, showcasing both his educational achievements and professional dedication.",
+        role: "Audit Senior at Deloitte",
+        desc: "Business Analytics at DePaul University, Chicago IL.",
         photo: "/images/Haider.jpg",
     },
     {
         name: "Zeeshan Ali",
-        role: "Augustana College",
-        desc: "Zeeshan Ali, originally from Chitral, Pakistan, received valuable support from HigherEd Global to pursue his academic aspirations at Augustana College, where he majored in Accounting and Finance. Zeeshan's commitment to practical learning is demonstrated through internships at UP Key and the YMCA. Today, he stands as an Audit and Assurance Senior at Deloitte, showcasing a seamless blend of academic excellence and professional success.",
+        role: "Audit Senior at Deloitte",
+        desc: "Accounting and Finance at Augustana College, Rock Island IL",
         photo: "/images/zeeshan.jpg",
     },
     {
         name: "Shayan Nadeem",
-        role: "Knox College",
-        desc: "Shayan Nadeem, originally from Lahore, Pakistan, found educational support through HigherEd Global to pursue his passion at Knox College. Majoring in Education, Shayan's commitment to his field is evident in his current role as an Archives Assistant at Knox College. His journey reflects a seamless transition from academic pursuit to a meaningful professional role, showcasing his dedication and expertise in the field of education.",
+        role: "Middle School Teacher in Chicago",
+        desc: "Education Major at Knox College, Galesburg IL.",
         photo: "/images/shayaan.jpg",
     },
     {
         name: "Osama Mushtaq",
-        role: "Tryine University",
-        desc: "Osama Mushtaq, originally from Peshawar, Pakistan, pursued his MS in Business Analytics at Trine University with the assistance of HigherEd Global. With prior experience as an Accounting Intern at Boost Mobile, he now excels in his role as an Audit & Assurance Senior Assistant at Deloitte, showcasing a seamless integration of academic and professional achievements.",
+        role: "Audit Senior at Deloitte",
+        desc: "Business Analytics at Trine University, Detroit MI.",
         photo: "/images/osama.jpg",
     },
     {
         name: "Shaheer Ahmad",
-        role: "Augustana College",
-        desc: "Shaheer Ahmed, originally from Kashmir, Pakistan, pursued a BBA with a focus on Accounting and Data Analytics at Augustana College with the support of HigherEd Global. With a background as an Internal Audit Intern at Royal Neighbors of America, Shaheer now thrives as Assurance Staff at EY, demonstrating a seamless integration of academic knowledge and professional success.",
+        role: "Audit Associate at EY",
+        desc: "Accounting and Data Analytics at Augustana College, Rock Island IL.",
         photo: "/images/shaheer.jpg",
     },
     ];
@@ -114,15 +114,6 @@ window.addEventListener("load", event => {
                             <p class="role">${role}</p> 
                             <p class="desc">${desc}</p> 
                         </article>
-                        
-                        <div class="social">
-                        <span class="pointer"></span>
-                        <div class="icons">
-                            <a class="icon" href="${website}" target="_blank" data-index="0"><img src="${iWebsite}"></a>
-                            <a class="icon" href="${email}" target="_blank" data-index="1"><img src="${iEmail}"></a>
-                            <a class="icon" href="${linkedin}" target="_blank" data-index="2"><img src="${iLinkedin}"></a>
-                            </div>
-                            </div>
                     </div>
                 </div>`;
 
@@ -218,7 +209,8 @@ window.addEventListener("load", event => {
     /* end */
 });
 
-const jobTitles = ["Engineer", "Artist", "Auditor", "Professor", "Teacher", "Coach", "PhD Candidate"];
+
+const jobTitles = ["Engineer", "Artist", "Auditor", "Professor", "Teacher", "Coach", "PhD Candidate", "Chemist", "Data Steward", "Consultant"];
 const jobTitleElement = document.getElementById("job-title");
 let currentIndex = 0;
 
@@ -237,7 +229,28 @@ function updateJobTitle() {
     currentIndex = (currentIndex + 1) % jobTitles.length;
 }
 
-setInterval(updateJobTitle, 2000); // Change job title and color every 2 seconds
+setInterval(updateJobTitle, 3000); // Change job title and color every 4 seconds
+
+//const jobTitles = ["Engineer", "Artist", "Auditor", "Professor", "Teacher", "Coach", "PhD Candidate"];
+//const jobTitleElement = document.getElementById("job-title");
+//let currentIndex = 0;
+
+//function getRandomColor() {
+//    const letters = '0123456789ABCDEF';
+//    let color = '#';
+//    for (let i = 0; i < 6; i++) {
+//        color += letters[Math.floor(Math.random() * 16)];
+//    }
+//    return color;
+//}
+
+//function updateJobTitle() {
+//    jobTitleElement.textContent = jobTitles[currentIndex];
+//    jobTitleElement.style.color = getRandomColor(); // Set random color
+//    currentIndex = (currentIndex + 1) % jobTitles.length;
+//}
+
+//setInterval(updateJobTitle, 2000); // Change job title and color every 2 seconds
 
 function smoothScroll(target) {
     var targetElement = document.querySelector(target);
@@ -250,24 +263,24 @@ function smoothScroll(target) {
     }
 }
 
-//var services = document.querySelectorAll('.service');
+var services = document.querySelectorAll('.service');
 
-//// Function to show each service one by one
-//function showServices(index) {
-//    if (index < services.length) {
-//        services[index].style.opacity = '1';
-//        setTimeout(function () {
-//            showServices(index + 1);
-//        }, 500); // Adjust the delay time (in milliseconds) between each section
-//    }
-//}
+// Function to show each service one by one
+function showServices(index) {
+    if (index < services.length) {
+        services[index].style.opacity = '1';
+        setTimeout(function () {
+            showServices(index + 1);
+        }, 500); // Adjust the delay time (in milliseconds) between each section
+    }
+}
 
-//// Initially hide all services
-//services.forEach(function (service) {
-//    service.style.opacity = '0';
-//});
+// Initially hide all services
+services.forEach(function (service) {
+    service.style.opacity = '0';
+});
 
-//// Trigger the function to show services when the page is loaded
-//document.addEventListener('DOMContentLoaded', function () {
-//    showServices(0);
-//});
+// Trigger the function to show services when the page is loaded
+document.addEventListener('DOMContentLoaded', function () {
+    showServices(0);
+});
